8ebf2df0d6b9864946e5955e82a9ec10c5200dd8
Ismael Garcia <ig@reflect-media.de>
2024-07-29 10:50:26 +0200

    Update the mobile light mode

--- packages/ui/components/content/HeroComponent.vue
+++ packages/ui/components/content/HeroComponent.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-
 /**
  *
  * The hero component
@@ -19,18 +18,18 @@ interface Props {
   href: string;
 }
 const props = defineProps<Props>();
-const { alt, heroImageUrl, subTitle, title,href } = toRefs(props);
+const { alt, heroImageUrl, subTitle, title, href } = toRefs(props);
 </script>
 
 <template>
   <section class="container mx-auto px-4 pb-12">
     <div
-      class="relative z-0 grid min-h-[700px] rounded-3xl md:bg-black bg-opacity-90 md:dark:bg-slate-800"
+      class="relative z-0 grid min-h-[700px] rounded-3xl md:bg-black bg-opacity-90 dark:bg-opacity-90 bg-slate-800 px-4 md:px-0 md:dark:bg-slate-800"
     >
       <img
         :src="heroImageUrl"
         class="absolute inset-0 h-full w-full rounded-3xl opacity-20 hidden lg:block"
-      >
+      />
       <div class="mb-20 mt-auto">
         <div class="relative rounded-3xl md:py-36 lg:px-12">
           <div class="-mx-4 mt-24 flex flex-wrap items-center space-y-6 lg:space-y-0">
